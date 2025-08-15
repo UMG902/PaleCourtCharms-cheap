8823974d10bd4ab803a44e7500f6a7d8a1dc7060
Roma-337 <123456789az4135@gmail.Com>
2025-08-15 22:07:21 +0300

    Allow daggers to multihit with shaman amp or snail soul
    (otherwise that synergy is almost useless)

--- Charms/Dagger.cs
+++ Charms/Dagger.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PaleCourtCharms
@@ -6,27 +8,198 @@ namespace PaleCourtCharms
     {
         private const int DaggerDamage = 13;
         private const int DaggerDamageUpgraded = 25;
-        public bool upgraded;
+        public bool upgraded = false;
+        public bool shamanEquipped = false;
+        public bool snailEquipped = false;
+
+        private readonly Dictionary<GameObject, Coroutine> _activeTargets = new Dictionary<GameObject, Coroutine>();
+
+        private bool _toggleSource = false;
+
+        private void OnDestroy()
+        {
+            foreach (var kv in new List<KeyValuePair<GameObject, Coroutine>>(_activeTargets))
+            {
+                var c = kv.Value;
+                SafeStopCoroutine(c);
+            }
+            _activeTargets.Clear();
+        }
 
         private void OnTriggerEnter2D(Collider2D collider)
         {
             int layer = collider.gameObject.layer;
-            if(layer == 20 || layer == 9 || layer == 26 || layer == 31 || collider.CompareTag("Geo"))
+            if (layer == 20 || layer == 9 || layer == 26 || layer == 31 || collider.CompareTag("Geo"))
             {
                 return;
             }
+
+            GameObject target = collider.gameObject;
+            if (target == null) return;
+
+            if (shamanEquipped || snailEquipped)
+            {
+                if (!_activeTargets.ContainsKey(target))
+                {
+                    IEnumerator routine = MultiHitTargetCoroutine(target);
+                    if (routine == null) return;
+
+                    Coroutine c = SafeStartCoroutine(routine);
+                    if (c != null)
+                    {
+                        _activeTargets[target] = c;
+                    }
+                }
+            }
+            else
+            {
+                DoHit(target);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            GameObject target = collider.gameObject;
+            if (target == null) return;
+
+            if (_activeTargets.TryGetValue(target, out Coroutine c))
+            {
+                SafeStopCoroutine(c);
+                _activeTargets.Remove(target);
+            }
+        }
+        private Coroutine SafeStartCoroutine(IEnumerator routine)
+        {
+            if (routine == null) return null;
+            if (!Application.isPlaying) return null;
+
+            try
+            {
+                if (GameManager.instance != null)
+                {
+                    var gm = GameManager.instance;
+                    if (gm != null && gm.gameObject != null)
+                    {
+                        try
+                        {
+                            return gm.StartCoroutine(routine);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+
+                if (this != null && gameObject != null && enabled && gameObject.activeInHierarchy)
+                {
+                    try
+                    {
+                        return StartCoroutine(routine);
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+            return null;
+        }
+        private void SafeStopCoroutine(Coroutine c)
+        {
+            if (c == null) return;
+            if (!Application.isPlaying) return;
+
+            try
+            {
+                if (GameManager.instance != null)
+                {
+                    try
+                    {
+                        GameManager.instance.StopCoroutine(c);
+                        return;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                if (this != null)
+                {
+                    try
+                    {
+                        StopCoroutine(c);
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+            catch
+            {
+             
+            }
+        }
+        private float GetMultiHitInterval()
+        {
+            if (shamanEquipped && snailEquipped) return 0.22f;
+            if (shamanEquipped) return 0.20f;
+            if (snailEquipped) return 0.24f;
+            return 0.22f; 
+        }
+
+        private IEnumerator MultiHitTargetCoroutine(GameObject target)
+        {
+            if (target == null) yield break;
+            DoHit(target);
+
+            float interval = GetMultiHitInterval();
+
+            while (target != null)
+            {
+                if (this == null) yield break;
+
+                yield return new WaitForSeconds(interval);
+
+                if (target == null) yield break;
+                if (this == null) yield break;
+
+                DoHit(target);
+
+                interval = GetMultiHitInterval();
+            }
+        }
+
+        private void DoHit(GameObject target)
+        {
+            if (target == null) return;
+
             HitInstance smallShotHit = new HitInstance();
             smallShotHit.DamageDealt = upgraded ? DaggerDamageUpgraded : DaggerDamage;
             smallShotHit.AttackType = AttackTypes.Spell;
             smallShotHit.IgnoreInvulnerable = true;
-            smallShotHit.Source = gameObject;
+
+
+            smallShotHit.Source = _toggleSource ? gameObject : null;
+            _toggleSource = !_toggleSource;
+
             smallShotHit.Multiplier = 1f;
-            //HealthManager hm = collider.gameObject.GetComponent<HealthManager>();
-            //if(hm != null) hm.Hit(smallShotHit);
 
-            HitTaker.Hit(collider.gameObject, smallShotHit, 3);
+            try
+            {
+                HitTaker.Hit(target, smallShotHit, 3);
+            }
+            catch
+            {
+
+            }
         }
-
-        
     }
 }
